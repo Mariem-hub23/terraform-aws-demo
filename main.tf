dae1e724e3479339c5f407efa91f6d1cc469590b
Mariem Ghali <mariem.ghali@dci-student.org>
2025-12-01 15:38:44 +0100

    adding a new tag value k:v

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 locals {
-    developer_name = "Mariem"
+  developer_name = "Mariem"
 }
 
 resource "aws_vpc" "my-vpc" {
@@ -9,12 +9,13 @@ resource "aws_vpc" "my-vpc" {
 
   tags = {
     Name = "${local.developer_name}-VPC"
+    env  = "prod_env"
   }
 }
 
 resource "aws_subnet" "my-subnet" {
-  vpc_id     = aws_vpc.my-vpc.id
-  cidr_block = var.subnet_cidr
+  vpc_id                  = aws_vpc.my-vpc.id
+  cidr_block              = var.subnet_cidr
   map_public_ip_on_launch = true
 
   tags = {
@@ -25,7 +26,7 @@ resource "aws_subnet" "my-subnet" {
 resource "aws_instance" "my-instance" {
   ami           = var.ami_id #"ami-0a6793a25df710b06"
   instance_type = var.instance_type
-  subnet_id = aws_subnet.my-subnet.id
+  subnet_id     = aws_subnet.my-subnet.id
 
   tags = {
     Name = "${local.developer_name}-instance"
@@ -33,9 +34,9 @@ resource "aws_instance" "my-instance" {
 }
 
 resource "aws_s3_bucket" "my-bucket" {
-  bucket =  var.bucket_name #"mariem-aws-bucket-1234"
+  bucket = var.bucket_name #"mariem-aws-bucket-1234"
 
   tags = {
-    Name        = "${local.developer_name}-S3-Bucket"
+    Name = "${local.developer_name}-S3-Bucket"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,30 +1,30 @@
 variable "aws_region" {
-    type = string
-    description = "this field hold aws region info"
-    default = "eu-central-1"
+  type        = string
+  description = "this field hold aws region info"
+  default     = "eu-central-1"
 }
 
 variable "vpc_cidr" {
-    type = string
-    description = "this field hold aws cidr block for the vpc"
-    default = "10.0.0.0/16"
+  type        = string
+  description = "this field hold aws cidr block for the vpc"
+  default     = "10.0.0.0/16"
 }
 
 variable "subnet_cidr" {
-    type = string
-    description = "this field hold aws cidr block for the subnet"
-    default = "10.0.1.0/24"
+  type        = string
+  description = "this field hold aws cidr block for the subnet"
+  default     = "10.0.1.0/24"
 }
 
 variable "ami_id" {
-    type = string
+  type = string
 }
 
 variable "instance_type" {
-    type = string
-    default = "t3.micro"
+  type    = string
+  default = "t3.micro"
 }
 
 variable "bucket_name" {
-    type = string
+  type = string
 }
